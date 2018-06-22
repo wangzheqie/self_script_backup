b473d465c9de6609410dc01053136402b0d46c7b
王哲 <wang_zhe@piec.com.cn>
2018-06-22 16:45:01 +0800

    t

---
+++ t.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2.7
+# -*- coding=utf-8 -*- 
+################################################################
+### > File Name: t.py
+### > Author: wangzheqie At https://github.com/wangzheqie
+### > Mail: wang_zhe@piec.com.cn
+### > Created Time: Fri 22 Jun 2018 03:56:45 PM CST
+################################################################
+import numpy as np
+import scipy as sci
+
+a = 8500.0 
+b = 5852.0 
+c = a - b
+a1 = b/22 * 20
+a2 = c/22 * 19
+print(a1, a2, a1+a2)
+print(a/23, c/23)
+print(0.75 * 22)
+
