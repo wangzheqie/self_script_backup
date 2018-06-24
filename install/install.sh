4b645d00f7e8b8faf9cdfdbc4d449d6e09a6bc75
Wang Zhe <wangzheqie@qq.com>
2018-06-24 23:36:10 +0800

    install

---
+++ install/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+
+#sudo mv /etc/apt/sources.list /etc/apt/sources.list.bk
+#sudo cp sources.list /etc/apt/sources.list
+
+# sudo apt-get update 
+# sudo apt-get upgrade 
+
+# sudo apt-get install -y git cmake gcc g++ libopencv-dev python-dev python3-dev
+
+# python pip 
+#pip install -i https://pypi.tuna.tsinghua.edu.cn/simple some-package
+#mkdir ~/.config/pip
+#echo "[global]" >> ~/.config/pip/pip.conf 
+#echo "index-url = https://pypi.tuna.tsinghua.edu.cn/simple" >> ~/.config/pip/pip.conf 
+
+
+
+
