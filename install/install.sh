182c885b9bb062344525663a288ea575071336f7
Wang Zhe <wangzheqie@qq.com>
2018-06-24 23:39:01 +0800

    bk

--- install/install.sh
+++ install/install.sh
@@ -8,12 +8,28 @@
 
 # sudo apt-get install -y git cmake gcc g++ libopencv-dev python-dev python3-dev
 
-# python pip 
+################################python pip
 #pip install -i https://pypi.tuna.tsinghua.edu.cn/simple some-package
 #mkdir ~/.config/pip
 #echo "[global]" >> ~/.config/pip/pip.conf 
 #echo "index-url = https://pypi.tuna.tsinghua.edu.cn/simple" >> ~/.config/pip/pip.conf 
 
 
+################################git 
+#git config --global user.email "wangzheqie@qq.com"
+#git config --global user.name "Wang Zhe"
+#mkdir ~/Git
+#cd Git 
+#git clone https://github.com/wangzheqie/self_script_backup.git
+#cd ~/Git/self_script_backup/install/
+#./install.sh
+#./update.sh
 
 
+
+
+
+
+################################at last 
+sudo apt-get update 
+sudo apt-get upgrade
