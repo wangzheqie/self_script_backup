262f875a27033deee95adfa235ef0ae170b93950
Wang Zhe <wangzheqie@qq.com>
2018-06-25 23:57:48 +0800

    test success

--- install/install.sh
+++ install/install.sh
@@ -3,13 +3,14 @@
 #sudo mv /etc/apt/sources.list /etc/apt/sources.list.bk
 #sudo cp sources.list /etc/apt/sources.list
 
-# sudo apt-get update 
-# sudo apt-get upgrade 
+#sudo apt-get update 
+#sudo apt-get upgrade 
 
-# sudo apt-get install -y git cmake gcc g++ libopencv-dev python-dev python3-dev
+#sudo apt-get install -y git vim cmake gcc g++ libopencv-dev python-dev python3-dev python-pip python3-pip
 
 ################################python pip
-#pip install -i https://pypi.tuna.tsinghua.edu.cn/simple some-package
+#pip install --upgrade pip
+#pip install -i https://pypi.tuna.tsinghua.edu.cn/simple numpy scipy opencv-python
 #mkdir ~/.config/pip
 #echo "[global]" >> ~/.config/pip/pip.conf 
 #echo "index-url = https://pypi.tuna.tsinghua.edu.cn/simple" >> ~/.config/pip/pip.conf 
@@ -19,7 +20,7 @@
 #git config --global user.email "wangzheqie@qq.com"
 #git config --global user.name "Wang Zhe"
 #mkdir ~/Git
-#cd Git 
+#cd ~/Git 
 #git clone https://github.com/wangzheqie/self_script_backup.git
 #cd ~/Git/self_script_backup/install/
 #./install.sh
@@ -27,18 +28,19 @@
 
 ################################vim
 #git clone https://github.com/chxuan/vimplus.git ~/.vimplus
-cd ~/.vimplus
-sudo ./install.sh
+#cd ~/.vimplus
+#sudo ./install.sh
 
 
 ################################ibus
-#sudo apt-get install ibus-pinyin 
-#sudo apt-get install ibus ibus-clutter ibus-gtk ibus-gtk3 ibus-qt4
+#sudo apt-get install -y ibus-pinyin 
+#sudo apt-get install -y ibus ibus-clutter ibus-gtk ibus-gtk3 ibus-qt4
 #im-switch -s ibus
-#sudo apt-get install ibus-pinyin
+#sudo apt-get install -y ibus-pinyin
 #/usr/lib/ibus/ibus-setup-pinyin
 #ibus-setup
 
 ################################at last 
-#sudo apt-get update 
-#sudo apt-get upgrade
+sudo apt-get update 
+sudo apt-get upgrade
+sudo apt autoremove -y
