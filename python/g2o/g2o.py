8c970bcf4be50fb0de2a1ef7e5938cbcf6db61fd
王哲 <wang_zhe@piec.com.cn>
2018-07-20 08:17:05 +0800

    g2o 0.1

---
+++ python/g2o/g2o.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import numpy as np 
+import numpy.linalg as nplg 
+
+class G2OError(Exception):
+    pass 
+
+class G2O(object):
+    def __init(self):
+        pass 
+
+
+
